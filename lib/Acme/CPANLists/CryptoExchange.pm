30a5d017e439baeb97cb23d52b9354ca5f834232
Steven Haryanto (on pc-office) <stevenharyanto@gmail.com>
2018-01-09 07:35:06 +0700

    Initial commit

---
+++ lib/Acme/CPANLists/CryptoExchange.pm
@@ -0,0 +1,47 @@
+package Acme::CPANLists::CryptoExchange;
+
+# DATE
+# VERSION
+
+our @Module_Lists = (
+    {
+        summary => "Modules that interface to cryptocurrency exchanges",
+        entries => [
+            {
+                module => 'WebService::Cryptopia',
+            },
+            {
+                module => 'WebService::Binance',
+            },
+            {
+                module => 'Finance::BTCIndo',
+            },
+            {
+                module => 'Finance::BitFlip',
+            },
+            {
+                module => 'Finance::Bank::Kraken',
+            },
+            {
+                module => 'Poloniex::API',
+            },
+            {
+                module => 'WWW::API::Bitfinex',
+            },
+            {
+                module => 'Finance::BitStamp::API',
+            },
+            {
+                module => 'Finance::GDAX::API',
+            },
+            {
+                module => 'Finance::LocalBitcoins::API',
+            },
+        ],
+    },
+);
+
+1;
+# ABSTRACT:
+
+=head1 DESCRIPTION
